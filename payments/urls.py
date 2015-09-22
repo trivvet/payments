56fa2cb70554a38f9b1f7beb6c043f8650fad768
Trivvet <trivvet@gmail.com>
2015-09-22 22:32:17 +0300

    v 0.2.1 add first form

--- payments/urls.py
+++ payments/urls.py
@@ -13,12 +13,13 @@ Including another URLconf
     1. Add an import:  from blog import urls as blog_urls
     2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
 """
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.contrib import admin
 
 from rent import views
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^$', views.home, name='home'),
-    url(r'^admin/', include(admin.site.urls), name='admin'),
-]
+    url(r'^add_product/$', views.add_product, name='add_product'),
+    url(r'^admin/', include(admin.site.urls), name='admin')
+)
--- rent/views.py
+++ rent/views.py
@@ -1,9 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Supply
+import pdb
 
 # Create your views here.
 
 def home(request):
-	supplies = Supply.objects.all()
+	if Supply.objects.count() > 0:
+		supplies = Supply.objects.all()
+		return render(request, 'rent/home.html', {'supplies': supplies})
+	else: 
+		return render(request, 'rent/home.html', {})
 	
 	return render(request, 'rent/home.html', {'supplies': supplies})
+	
+def add_product(request):
+	if request.method == 'POST':
+		return HttpResponse(u'<h2>Форма додавання послуги<h2>')
+	else:
+		return render(request, 'rent/add_product.html', {})
