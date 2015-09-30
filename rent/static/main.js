a7ac8501f009089f3919d2cd14e7c2997a3cf041
Trivvet <trivvet@gmail.com>
2015-09-30 14:51:59 +0300

    v 0.4.1 add counters jquery

--- rent/static/main.js
+++ rent/static/main.js
@@ -1,35 +1,71 @@
 function initAddForm() {
   $('option').click(function(event) {
     var option = $(this);
-    $('div.provider').removeAttr('hidden');
-    $('p.provider').attr('hidden', 'true');
-    $('a.provider_site').attr('hidden', 'true');
+    $('div.provider').show();
+    $('#add_counter').removeAttr('disabled');
+    $('#inputArrears').removeAttr('disabled');
+    $('p.provider').hide();
+    $('a.provider_site').hide();
     switch($(this).val()) {
 	  case 'water':
 	  case 'sewerage':
-	    $('.water').removeAttr('hidden');
+	    $('.water').show();
 	    $('#inputProvider').val('Хмельницькводоканал');
 	    $('#inputSiteProvider').val('water.km.ua');
 	    break;
 	  case 'electricity':
-	    $('.electricity').removeAttr('hidden');
+	    $('.electricity').show();
 	    $('#inputProvider').val('Хмельницькобленерго');
 	    $('#inputSiteProvider').val('oblenergo.km.ua/portal/');
 	    break;
 	  case 'gas':
-		$('.gas').removeAttr('hidden');
+		$('.gas').show();
 		$('#inputProvider').val('Хмельницькгаз');
 		$('#inputSiteProvider').val('km.104.ua/ua/');
 		break;
       case 'waste':
-        $('.waste').removeAttr('hidden');
+        $('.waste').show()
         $('#inputProvider').val('Спецкомунтранс');
         $('#inputSiteProvider').val('skt.km.ua/');
+        $('#add_counter').attr('disabled', 'true');
         break;
 	  default:
-	    $('div.provider').attr('hidden', 'true');
+	    $('div.provider').hide();
+        $('#inputArrears').attr('disabled', 'true');
 	    break;
 	};
+  });
+  $('#add_counter').click(function(event) {
+    var button = $(this);
+    if ($('div.counter1').attr('hidden')) {
+      $('div.counter1').removeAttr('hidden');
+    } else if ($('div.counter2').attr('hidden')) {
+      $('div.counter2').removeAttr('hidden');
+      $('#delete_counter_1').attr('disabled', 'true'); 
+    } else if ($('div.counter3').attr('hidden')) {
+      $('div.counter3').removeAttr('hidden');
+      $('#delete_counter_2').attr('disabled', 'true');
+    } else if ($('div.counter4').attr('hidden')) {
+      $('div.counter4').removeAttr('hidden');
+      $('#delete_counter_3').attr('disabled', 'true'); 
+      button.hide();
+    };
+  });
+  $('#delete_counter_1').click(function(event) {
+    $('div.counter1').attr('hidden', 'true');
+  });
+  $('#delete_counter_2').click(function(event) {
+    $('div.counter2').attr('hidden', 'true');
+    $('#delete_counter_1').removeAttr('disabled');
+  });
+  $('#delete_counter_3').click(function(event) {
+    $('div.counter3').attr('hidden', 'true');
+    $('#delete_counter_2').removeAttr('disabled');
+  });
+  $('#delete_counter_4').click(function(event) {
+    $('div.counter4').attr('hidden', 'true');
+    $('#delete_counter_3').removeAttr('disabled');
+    $('#add_counter').show(); 
   });
 }
 
